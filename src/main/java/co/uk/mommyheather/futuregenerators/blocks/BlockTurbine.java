26dbfd1b9d058ddeaab99bf0deb6066b0bda6198
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-10-09 22:34:59 +0100

    Proper model for the turbine

--- src/main/java/co/uk/mommyheather/futuregenerators/blocks/BlockTurbine.java
+++ src/main/java/co/uk/mommyheather/futuregenerators/blocks/BlockTurbine.java
@@ -5,18 +5,26 @@ import javax.annotation.Nullable;
 import co.uk.mommyheather.futuregenerators.tile.TileTurbine;
 import co.uk.mommyheather.futuregenerators.tile.Tiles;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
 public class BlockTurbine extends Block implements EntityBlock {
+    
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public BlockTurbine(Properties p_49795_) {
         super(p_49795_);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -28,6 +36,16 @@ public class BlockTurbine extends Block implements EntityBlock {
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
         return type == Tiles.turbine.get() ? TileTurbine::tick : null;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {
+        state.add(FACING);
+    }
+    
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext p_196258_1_) {
+        return this.defaultBlockState().setValue(FACING, p_196258_1_.getHorizontalDirection().getOpposite());
     }
 
 }
