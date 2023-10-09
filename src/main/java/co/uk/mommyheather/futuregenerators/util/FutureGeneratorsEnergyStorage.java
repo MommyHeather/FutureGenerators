43e946733b98a6889034132e8ef715ff933f0bb8
RaVen <66441550+RaVenInTheDark@users.noreply.github.com>
2023-10-09 19:34:19 +0100

    custom energy storage

---
+++ src/main/java/co/uk/mommyheather/futuregenerators/util/FutureGeneratorsEnergyStorage.java
@@ -0,0 +1,29 @@
+package co.uk.mommyheather.futuregenerators.util;
+
+import net.minecraftforge.energy.EnergyStorage;
+
+public class FutureGeneratorsEnergyStorage extends EnergyStorage{
+
+    public FutureGeneratorsEnergyStorage(int capacity) {
+        super(capacity, Integer.MAX_VALUE);
+    }
+
+    public FutureGeneratorsEnergyStorage(int capacity, int maxTransfer) {
+        super(capacity, maxTransfer);
+    }
+
+    public FutureGeneratorsEnergyStorage(int capacity, int maxReceive, int maxExtract) {
+        super(capacity, maxReceive, maxExtract);
+    }
+
+    public FutureGeneratorsEnergyStorage(int capacity, int maxReceive, int maxExtract, int energy) {
+        super(capacity, maxReceive, maxExtract, energy);
+    }
+
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    
+}
