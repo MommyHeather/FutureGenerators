74915983d5fe79d65444b4b535c2c0753ac5af1c
Heather White <66441550+MommyHeather@users.noreply.github.com>
2023-11-08 13:36:21 +0000

    Boiler block and be

---
+++ src/main/java/co/uk/mommyheather/futuregenerators/blocks/BlockBoiler.java
@@ -0,0 +1,98 @@
+package co.uk.mommyheather.futuregenerators.blocks;
+
+import javax.annotation.Nullable;
+
+import co.uk.mommyheather.futuregenerators.tile.TileBoiler;
+import co.uk.mommyheather.futuregenerators.tile.TileBoiler;
+import co.uk.mommyheather.futuregenerators.tile.Tiles;
+import co.uk.mommyheather.futuregenerators.ui.BoilerMenu;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
+
+public class BlockBoiler extends Block implements EntityBlock {
+    
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
+    public BlockBoiler(Properties p_49795_) {
+        super(p_49795_);
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new TileBoiler(pos, state);
+    }
+    
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type) {
+        return type == Tiles.boiler.get() ? TileBoiler::tick : null;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {
+        state.add(FACING);
+    }
+    
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext p_196258_1_) {
+        return this.defaultBlockState().setValue(FACING, p_196258_1_.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult trace) {
+        if (!level.isClientSide) {
+            BlockEntity be = level.getBlockEntity(pos);
+            if (be instanceof TileBoiler) {
+                MenuProvider containerProvider = new MenuProvider() {
+                    @Override
+                    public Component getDisplayName() {
+                        return Component.translatable("futuregenerators.ui.boiler");
+                    }
+
+                    @Override
+                    public AbstractContainerMenu createMenu(int windowId, Inventory playerInventory, Player playerEntity) {
+                        return new BoilerMenu(windowId, playerEntity, pos);
+                    }
+                };
+                NetworkHooks.openScreen((ServerPlayer) player, containerProvider, be.getBlockPos());
+            }
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    public void onRemove(BlockState state, Level level, BlockPos pos, BlockState oldState, boolean p_51542_) {
+        if (!state.is(oldState.getBlock())) {
+            TileBoiler boiler = (TileBoiler) level.getBlockEntity(pos);
+            for (int i=0; i<boiler.items.getSlots();i++) {
+                if (!boiler.items.getStackInSlot(i).isEmpty()) {
+                    Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), boiler.items.getStackInSlot(i));
+                }
+            }
+        }
+        super.onRemove(state, level, pos, oldState, p_51542_);
+    }
+
+}
---
+++ src/main/java/co/uk/mommyheather/futuregenerators/tile/TileBoiler.java
@@ -0,0 +1,248 @@
+package co.uk.mommyheather.futuregenerators.tile;
+
+import java.util.HashMap;
+import java.util.function.Predicate;
+
+import org.jetbrains.annotations.NotNull;
+
+import co.uk.mommyheather.futuregenerators.config.FutureGeneratorsConfig;
+import co.uk.mommyheather.futuregenerators.util.FutureGeneratorsEnergyStorage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+public class TileBoiler extends BlockEntity {
+
+    public DoubleTank tank;
+    public ItemStackHandler items;
+
+    private boolean ticked = false;
+
+    private LazyOptional<IFluidHandler> lazyTank;
+    private LazyOptional<IItemHandler> lazyItems;
+
+    private int ticks = 0;
+
+    public int fuelRemaining = 0;
+
+
+    @Override
+    public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            return lazyItems.cast();
+        }
+        if (cap == ForgeCapabilities.FLUID_HANDLER) {
+            return lazyTank.cast();
+        }
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+        public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyTank.invalidate();
+        lazyItems.invalidate();
+    }
+
+
+    public TileBoiler(BlockPos p_155229_, BlockState p_155230_) {
+        super(Tiles.boiler.get(), p_155229_, p_155230_);
+
+        tank = new DoubleTank(0, (stack) -> {
+            return stack.getFluid().isSame(Fluids.WATER);
+        }) {
+            @Override
+            protected void onContentsChanged()
+            {
+                setChanged();
+            }
+        };
+        
+        items = new ItemStackHandler(1) {          
+            @Override
+            public boolean isItemValid(int slot, @NotNull ItemStack stack)
+            {
+                return ForgeHooks.getBurnTime(stack, RecipeType.SMELTING) > 0;
+            }
+            
+            @Override
+            public void onContentsChanged(int slot) {
+                setChanged();
+            }
+        };
+
+        lazyTank = LazyOptional.of(() -> tank);
+        lazyItems = LazyOptional.of(() -> items);
+    }
+
+    
+    public static void tick(Level level, BlockPos pos, BlockState state, BlockEntity be) {
+        if (level.isClientSide) return;
+        TileBoiler boiler = (TileBoiler) be;
+
+
+        if (!boiler.ticked) {
+            boiler.tank.setCapacity(FutureGeneratorsConfig.SERVER.boilerWaterCapacity.get(), 2);
+
+
+            boiler.ticked = true;
+        }
+
+        boiler.ticks++;
+
+        if (boiler.fuelRemaining <= 0) {
+            if (boiler.tank.inTank.getFluidAmount() > 0 && boiler.tank.outTank.getFluidAmount() < boiler.tank.outTank.getCapacity()) {
+                //Only start a new burn under three conditions - burn time is empty, water tank is not empty, and hot water tank is not full
+                ItemStack stack = boiler.items.extractItem(0, 1, false);
+                boiler.fuelRemaining = ForgeHooks.getBurnTime(stack, RecipeType.SMELTING);
+            }
+        }
+        else {
+            boiler.fuelRemaining--; // burns fuel
+            boiler.tank.drainIntake(FutureGeneratorsConfig.SERVER.boilerHeatingRate.get());
+            boiler.tank.fillOutput(new FluidStack(co.uk.mommyheather.futuregenerators.fluids.Fluids.HOT_WATER.get(), FutureGeneratorsConfig.SERVER.boilerHeatingRate.get()));
+        }
+
+    }
+    
+    @Override
+    protected void saveAdditional(CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.put("tank", tank.writeToNBT(new CompoundTag()));
+        tag.put("items", items.serializeNBT());
+
+        tag.putInt("burnTime", fuelRemaining);
+
+    }
+
+    @Override
+    public void load(CompoundTag tag) {
+        super.load(tag);
+        if (tag.contains("tank")) {
+            tank.readFromNBT(tag.getCompound("tank"));
+        }
+        if (tag.contains("items")) {
+            items.deserializeNBT(tag.getCompound("items"));
+        }
+        if (tag.contains("burnTime")) {
+            this.fuelRemaining = tag.getInt("burnTime");
+        }
+    }
+
+
+
+    public static class DoubleTank implements IFluidHandler {
+        
+        private FluidTank inTank;
+        private FluidTank outTank;
+        
+        public DoubleTank(int capacity, Predicate<FluidStack> validatorInsert)
+        {
+            this.inTank = new FluidTank(capacity, validatorInsert);
+            this.outTank = new FluidTank(capacity);
+        }
+
+        @Override
+        public int getTanks() {
+            return 2;
+        }
+
+        @Override
+        public @NotNull FluidStack getFluidInTank(int tank) {
+            return tank == 0? inTank.getFluid() : outTank.getFluid();
+        }
+
+        @Override
+        public int getTankCapacity(int tank) {
+            return inTank.getCapacity();
+        }
+
+        @Override
+        public boolean isFluidValid(int tank, @NotNull FluidStack stack) {
+            return tank == 0? inTank.isFluidValid(stack) : outTank.isFluidValid(stack);
+        }
+
+        @Override
+        public int fill(FluidStack resource, FluidAction action) {
+            onContentsChanged();
+            return inTank.fill(resource, action);
+        }
+
+        @Override
+        public @NotNull FluidStack drain(FluidStack resource, FluidAction action) {
+            onContentsChanged();
+            return outTank.drain(resource, action);
+        }
+
+        @Override
+        public @NotNull FluidStack drain(int maxDrain, FluidAction action) {
+            onContentsChanged();
+            return outTank.drain(maxDrain, action);
+        }
+
+        //mod-only methods
+
+        public void drainIntake(int toDrain) {
+            onContentsChanged();
+            inTank.drain(toDrain, FluidAction.EXECUTE);
+        }
+
+        public void fillOutput(FluidStack resource) {
+            onContentsChanged();
+            outTank.fill(resource, FluidAction.EXECUTE);
+        }
+
+        
+        public void setCapacity(int capacity, int outMult)
+        {
+            inTank.setCapacity(capacity);
+            outTank.setCapacity(capacity * outMult);
+        }
+
+        protected void onContentsChanged() {
+
+        }
+
+        
+        public void readFromNBT(CompoundTag compound) {
+            if (compound.contains("in")) {
+                inTank.readFromNBT(compound.getCompound("in"));
+            }
+            if (compound.contains("out")) {
+                outTank.readFromNBT(compound.getCompound("out"));
+            }
+        }
+
+        public Tag writeToNBT(CompoundTag tag) {
+            CompoundTag in = new CompoundTag();
+            inTank.writeToNBT(in);
+            tag.put("in", in);
+            
+            CompoundTag out = new CompoundTag();
+            outTank.writeToNBT(out);
+            tag.put("out", out);
+
+            return tag;
+        }
+
+    }
+}
